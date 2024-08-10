f4eb9922e6a93cee6f289ea1cf92637a3a2f1976
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-08-11 02:20:13 +0530

    OOPS_ASS-Q7-SOL.cpp
    OOPS_ASS-Q7-SOL.cpp

---
+++ OOPS_ASS-Q7-SOL.cpp
@@ -0,0 +1,14 @@
+/*7. Which of the following statements is correct about the formal parameters
+in C++?
+a) Parameters with which functions are called
+b) Parameters which are used in the definition of the function
+c) Variables other than passed parameters in a function
+d) Variables that are never used in the function.
+
+---->
+
+The correct statement about formal parameters in C++ is:
+b) Parameters which are used in the definition of the function
+
+Explanation:
+Formal parameters are the variables that appear in the function's definition. They act as placeholders for the actual values (arguments) that will be passed to the function when it is called.*/
