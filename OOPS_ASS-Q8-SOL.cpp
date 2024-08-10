17c7c979434af445f430ca531928d5882f1d7160
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-08-11 02:24:49 +0530

    OOPS_ASS-Q8-SOL.cpp
    OOPS_ASS-Q8-SOL.cpp

---
+++ OOPS_ASS-Q8-SOL.cpp
@@ -0,0 +1,7 @@
+/*The correct option that gives the 4th element of the array is:
+c) Array[3];
+
+Explanation:
+    In C++ (and most programming languages that use zero-based indexing),
+    the index of the first element in an array is 0.
+    Therefore, the 4th element in the array is at index 3.*/
